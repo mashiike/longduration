28490b5b40be5ae01be4070ebab3aa335cca54d1
mashiike <ikeda-masashi@kayac.com>
2022-11-22 19:42:34 +0900

    implement additional strings

--- example_test.go
+++ example_test.go
@@ -7,19 +7,29 @@ import (
 )
 
 func ExampleParseDuration() {
+	years, _ := longduration.ParseDuration("1y")
+	weeks, _ := longduration.ParseDuration("4w")
 	days, _ := longduration.ParseDuration("3d")
 	hours, _ := longduration.ParseDuration("25h")
 	complex, _ := longduration.ParseDuration("1d2h34m10s")
+	fmt.Println(longduration.DurationString(years))
+	fmt.Println(longduration.DurationString(weeks, longduration.WithWeek()))
 	fmt.Println(longduration.DurationString(days))
 	fmt.Println(longduration.DurationString(hours))
 	fmt.Println(longduration.DurationString(complex))
+	fmt.Printf("There are %.4f weeks in %s.\n", longduration.Weeks(years), longduration.DurationString(years, longduration.WithYear()))
+	fmt.Printf("There are %.0f days in %s.\n", longduration.Days(weeks), longduration.DurationString(weeks, longduration.WithWeek()))
 	fmt.Printf("There are %.0f hours in %s.\n", days.Hours(), longduration.DurationString(days))
 	fmt.Printf("There are %.0f hours in %s.\n", hours.Hours(), longduration.DurationString(hours))
 	fmt.Printf("There are %.0f seconds in %s.\n", complex.Seconds(), longduration.DurationString(complex))
 	// Output:
+	// 365d
+	// 4w
 	// 3d
 	// 1d1h
 	// 1d2h34m10s
+	// There are 52.1429 weeks in 1y.
+	// There are 28 days in 4w.
 	// There are 72 hours in 3d.
 	// There are 25 hours in 1d1h.
 	// There are 95650 seconds in 1d2h34m10s.
--- string.go
+++ string.go
@@ -6,31 +6,107 @@ import (
 	"time"
 )
 
-// DurationString returns a string representation of the string Duration that also takes into account the number of days.
-func DurationString(d time.Duration) string {
-	days := uint64(d.Truncate(24*time.Hour).Hours() / 24.0)
-	remining := d - d.Truncate(24*time.Hour)
-	hours := uint64(remining.Truncate(time.Hour).Hours())
-	remining = remining - remining.Truncate(time.Hour)
-	minutes := uint64(remining.Truncate(time.Minute).Minutes())
-	remining = remining - remining.Truncate(time.Minute)
-	seconds := uint64(remining.Truncate(time.Second).Seconds())
-	return durationString(days, hours, minutes, seconds)
+type DuraitonStringOptions struct {
+	ShowWeek bool
+	ShowYear bool
 }
 
-func durationString(day, hours, minutes, seconds uint64) string {
-	var builder strings.Builder
-	if day > 0 {
-		fmt.Fprintf(&builder, "%dd", day)
+// DurationString returns a string representation of the string Duration that also takes into account the number of days.
+func DurationString(d time.Duration, optFns ...func(*DuraitonStringOptions)) string {
+	opts := &DuraitonStringOptions{
+		ShowYear: false,
+		ShowWeek: false,
 	}
-	if hours > 0 {
+	for _, optFn := range optFns {
+		optFn(opts)
+	}
+
+	var builder strings.Builder
+	remining := d
+	if remining.Seconds() < 0 {
+		fmt.Fprint(&builder, "-")
+		remining = remining.Abs()
+	}
+	var years, weeks uint64
+	if opts.ShowYear {
+		years = uint64(Years(remining.Truncate(Year)))
+		remining = remining - remining.Truncate(Year)
+		if years > 0 {
+			fmt.Fprintf(&builder, "%dy", years)
+		}
+		if remining == 0 {
+			return builder.String()
+		}
+	}
+	if opts.ShowWeek {
+		weeks = uint64(Weeks(remining.Truncate(Week)))
+		remining = remining - remining.Truncate(Week)
+		if years > 0 || weeks > 0 {
+			fmt.Fprintf(&builder, "%dw", weeks)
+		}
+		if remining == 0 {
+			return builder.String()
+		}
+	}
+	days := uint64(Days(remining.Truncate(Day)))
+	remining = remining - remining.Truncate(Day)
+	if years > 0 || weeks > 0 || days > 0 {
+		fmt.Fprintf(&builder, "%dd", days)
+	}
+	if remining == 0 {
+		return builder.String()
+	}
+	hours := uint64(remining.Truncate(time.Hour).Hours())
+	remining = remining - remining.Truncate(time.Hour)
+	if years > 0 || weeks > 0 || days > 0 || hours > 0 {
 		fmt.Fprintf(&builder, "%dh", hours)
 	}
-	if minutes > 0 {
+	if remining == 0 {
+		return builder.String()
+	}
+	minutes := uint64(remining.Truncate(time.Minute).Minutes())
+	remining = remining - remining.Truncate(time.Minute)
+	if years > 0 || weeks > 0 || days > 0 || hours > 0 || minutes > 0 {
 		fmt.Fprintf(&builder, "%dm", minutes)
 	}
-	if seconds > 0 {
-		fmt.Fprintf(&builder, "%ds", seconds)
+	if remining == 0 {
+		return builder.String()
 	}
+	fmt.Fprint(&builder, remining.String())
 	return builder.String()
+}
+
+// WithYear is an option to the DurationString function that also displays the Year.
+func WithYear() func(*DuraitonStringOptions) {
+	return func(opts *DuraitonStringOptions) {
+		opts.ShowYear = true
+	}
+}
+
+// WithWeek is an option to the DurationString function that also displays the week.
+func WithWeek() func(*DuraitonStringOptions) {
+	return func(opts *DuraitonStringOptions) {
+		opts.ShowWeek = true
+	}
+}
+
+// Years calculates the number of years to be rounded down
+func Years(d time.Duration) float64 {
+	years := d / Year
+	remining := (d - d.Truncate(Year)).Hours() / (float64(Year) / float64(time.Hour))
+	return float64(years) + float64(remining)
+}
+
+// Weeks calculates the number of weeks to be rounded down
+func Weeks(d time.Duration) float64 {
+	weeks := d / Week
+	remining := (d - d.Truncate(Week)).Hours() / (float64(Week) / float64(time.Hour))
+	return float64(weeks) + float64(remining)
+}
+
+// Days calculates the number of days to be rounded down
+func Days(d time.Duration) float64 {
+	days := d / Day
+	remining := (d - d.Truncate(Day)).Hours() / (float64(Day) / float64(time.Hour))
+	return float64(days) + float64(remining)
 }
