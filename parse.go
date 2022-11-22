5a1960a439f568bd35391e13d042b2604d0eb19b
mashiike <ikeda-masashi@kayac.com>
2022-11-22 17:58:33 +0900

    implement year and refactor

--- parse.go
+++ parse.go
@@ -7,22 +7,33 @@ import (
 
 // ParseDuration parses a duration string; it differs from the standard golang package in that it allows date expressions that do not take into account daylight saving time, etc. For example, `1d` is treated as 24 hours.
 func ParseDuration(str string) (time.Duration, error) {
-	week, str := trimWeek(str)
+	years, str := trim(str, 'y', 365*24)
 	if str == "" {
-		return week, nil
+		if years == 0 {
+			return 0, errors.New("invalid format")
+		}
+		return years, nil
 	}
-	days, parts := trimDay(str)
-	if parts != "" {
-		d, err := time.ParseDuration(parts)
-		return week + days + d, err
+	weeks, str := trim(str, 'w', 7*24)
+	if str == "" {
+		if years+weeks == 0 {
+			return 0, errors.New("invalid format")
+		}
+		return years + weeks, nil
 	}
-	if days == 0 {
-		return 0, errors.New("invalid format")
+	days, str := trim(str, 'd', 24)
+	if str == "" {
+		if years+weeks+days == 0 {
+			return 0, errors.New("invalid format")
+		}
+		return years + weeks + days, nil
 	}
-	return days, nil
+	d, err := time.ParseDuration(str)
+	return years + weeks + days + d, err
+
 }
 
-func trimDay(str string) (time.Duration, string) {
+func trim(str string, unit rune, coefficient int64) (time.Duration, string) {
 	var val int64
 	for i, c := range str {
 		if '0' <= c && c <= '9' {
@@ -30,24 +41,8 @@ func trimDay(str string) (time.Duration, string) {
 			val = val*10 + v
 			continue
 		}
-		if c == 'd' {
-			return time.Duration(val) * 24 * time.Hour, str[i+1:]
-		}
-		return 0, str
-	}
-	return 0, str
-}
-
-func trimWeek(str string) (time.Duration, string) {
-	var val int64
-	for i, c := range str {
-		if '0' <= c && c <= '9' {
-			v := int64(c - '0')
-			val = val*10 + v
-			continue
-		}
-		if c == 'w' {
-			return time.Duration(val) * 7 * 24 * time.Hour, str[i+1:]
+		if c == unit {
+			return time.Duration(val*coefficient) * time.Hour, str[i+1:]
 		}
 		return 0, str
 	}
--- parse_test.go
+++ parse_test.go
@@ -30,12 +30,24 @@ func TestParseDurationSuccess(t *testing.T) {
 			expected: 7 * 24 * time.Hour,
 		},
 		{
+			str:      "1y",
+			expected: 365 * 24 * time.Hour,
+		},
+		{
 			str:      "1d1m3s",
 			expected: 24*time.Hour + time.Minute + 3*time.Second,
 		},
 		{
 			str:      "1w1d1m3s",
-			expected: 8*24*time.Hour + time.Minute + 3*time.Second,
+			expected: 7*24*time.Hour + 24*time.Hour + time.Minute + 3*time.Second,
+		},
+		{
+			str:      "1y1w1d1m3s",
+			expected: 365*24*time.Hour + 7*24*time.Hour + 24*time.Hour + time.Minute + 3*time.Second,
+		},
+		{
+			str:      "1y3w1d",
+			expected: 365*24*time.Hour + 3*7*24*time.Hour + 24*time.Hour,
 		},
 	}
 
@@ -72,6 +84,12 @@ func TestParseDurationFailed(t *testing.T) {
 		},
 		{
 			str: "d",
+		},
+		{
+			str: "y",
+		},
+		{
+			str: "w",
 		},
 	}
 
